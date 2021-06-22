5f79a5f3356598ede8eefcd515ddd3639e2bd04a
Govish <9davidmuia@gmail.com>
2021-06-22 17:01:28 +0300

    working tokens

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('perm');
+            $table->string('perm')->nullable()->default('user');
+            $table->string('api_token', 80)
+                ->unique()
+                ->nullable()
+                ->default(null);
             $table->rememberToken();
             $table->timestamps();
         });
