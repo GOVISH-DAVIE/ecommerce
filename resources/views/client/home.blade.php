600287ecdebadf4e2d889a8f943e059e34c553e1
Govish <9davidmuia@gmail.com>
2021-06-24 16:43:25 +0300

    Contact Form Done

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -85,25 +85,55 @@
 
 
 
- 
+
     <div class="container  ">
-            <div class="card contactForm">
+        <div class="card contactForm">
+            <h1><u> Contact</u></h1>
+            <h2> Nairobi -Kenya</h2>
+            <h4>
+                damimaplans@gmail.com
+            </h4>
+
+
+            <h4>0726143237</h4>
+
+            <h4> migwi81@gmail.com</h4>
+            </h4>
+            <div class=" ">
                 <form>
+                 <div class="cotainer">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group col-sm-12">
+                                <label for="exampleInputEmail1">Name</label>
+                                <input type="Text" class="form-control col-sm-12" id="exampleInputEmail1" aria-describedby="emailHelp"
+                                    placeholder="Full Name">
+    
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group col-sm-12" >
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control col-sm-12" id="exampleInputEmail1" aria-describedby="emailHelp"
+                                    placeholder="Enter email">
+    
+                            </div>
+                        </div>
+                     </div>
+                 </div>
                     <div class="form-group">
-                      <label for="exampleInputEmail1">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        <label for="exampleInputPassword1">Subject</label>
+                        <input type="Text" class="form-control" id="exampleInputPassword1" placeholder="Subject">
                     </div>
                     <div class="form-group">
-                      <label for="exampleInputPassword1">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <label for="exampleInputPassword1">Subject</label>
+                        <textarea type="Text" class="form-control" id="exampleInputPassword1" placeholder="Type your message here">
+                            </textarea>
                     </div>
-                    <div class="form-check">
-                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
+                     
                     <button type="submit" class="btn btn-primary">Submit</button>
-                  </form>
+                </form>
             </div>
-    </div> 
+        </div>
+    </div>
 @endsection
