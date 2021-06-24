a2ee4070ac61a8c10cc5c43af71b0a09ef927077
Govish <9davidmuia@gmail.com>
2021-06-24 15:44:24 +0300

    asssets ..slider

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -26,4 +26,60 @@
             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </div>
     </div>
+
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-5 sliderText">
+                            <p>
+                                Good buildings come from good people, and all problems are solved by good design
+                            </p>
+                        </div>
+                        <div class="col-sm-12 col-md-5 sliderText ">
+                            <img src="/images/1.webp" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-5 sliderText">
+                            <p>
+                                Good buildings come from good people, and all problems are solved by good design
+                            </p>
+                        </div>
+                        <div class="col-sm-12 col-md-5 sliderText ">
+                            <img src="/images/2.webp" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-5 sliderText">
+                            <p>
+                                Good buildings come from good people, and all problems are solved by good design
+                            </p>
+                        </div>
+                        <div class="col-sm-12 col-md-5 sliderText ">
+                            <img src="/images/2.webp" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 @endsection
