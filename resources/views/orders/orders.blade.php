8198088558d023c683d6bf5b8b2d5c78a199442d
Govish <9davidmuia@gmail.com>
2021-07-09 01:27:26 +0300

    Orders Done

--- resources/views/orders/orders.blade.php
+++ resources/views/orders/orders.blade.php
@@ -3,37 +3,31 @@
 @section('content')
     <div class="container-fluid">
         <div class="row ">
-
-          {{json_encode($orders)}}
-
+ 
           <table class="table">
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">User Name</th>
+                <th scope="col">User Email</th>
+                <th scope="col">Amount</th>
+                <th scope="col">View Property</th>
+                <th scope="col">Property Title</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
+             @foreach ($orders as $item)
+             <tr>
                 <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
+                <td>{{$item->user->name}}</td>
+                <td>{{ $item->user->email}}</td>
+                <td>{{$item->amount }}</td>
+                <td>
+                <a href="/index/{{$item->property_id }}" class="btn btn-primary"> view</a>
+                </td>
+                <td>{{$item->property->Title }}</td>
+              </tr> 
+             @endforeach
             </tbody>
           </table>
 
